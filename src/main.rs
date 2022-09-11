08de9676b997d04cbb27354284b3cdce3f5df77b
Laegel <valentin.chouaf@laposte.net>
2022-09-11 18:56:53 +0200

    r&d(trainer): Added a richer s_exp representation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, path::PathBuf, io::BufReader};
+use std::{fs::{self, File}, io::BufReader, path::PathBuf};
 
 use tree_sitter::{Language, Parser, Query, QueryCapture, QueryCursor, Node};
 mod utils;
@@ -6,7 +6,7 @@ use utils::GrammarJSON;
 
 use once_cell::sync::Lazy;
 
-static GRAMMAR: Lazy<String> = Lazy::new(||get_grammar());
+static GRAMMAR: Lazy<GrammarJSON> = Lazy::new(||get_grammar());
 
 
 extern "C" {
@@ -16,10 +16,26 @@ extern "C" {
     fn tree_sitter_rust() -> Language;
 }
 
+trait Sexp2 {}
+
+impl<'tree> Sexp2 for Node<'tree> {
+    fn to_sexp2(&self) -> String {
+        let c_string = unsafe { ffi::ts_node_string(self.0) };
+        let result = unsafe { CStr::from_ptr(c_string) }
+            .to_str()
+            .unwrap()
+            .to_string();
+        unsafe { (FREE_FN)(c_string as *mut c_void) };
+        result
+    }
+}
+
 fn main() {
     let mut parser = Parser::new();
     // Store some source code in an array of lines.
-    let lines = "pub fn foo() {1}".as_bytes();
+    let source = fs::read_to_string("./src/main.rs").expect("Could not find file");
+    // let source = "pub fn foo() {1}pub fn bar() {2}";
+    let lines = source.as_bytes();
     let language = unsafe { tree_sitter_rust() };
     parser.set_language(language).unwrap();
 
@@ -29,22 +45,68 @@ fn main() {
 
     // (source_file (function_item (visibility_modifier) name: (identifier) parameters: (parameters) body: (block (integer_literal))))
 
-    println!("{}", tree.root_node().to_sexp());
+    println!("{:?}", tree.root_node().to_sexp());
+    let print_node =|node: Node| {
+        // println!("{}: {}", node.kind(), &source[node.start_byte()..node.end_byte()]);
+    };
+    
+    let mut cursor = tree.walk();
+    loop {
 
+        // Keep travelling down the tree as far as we can
+        if cursor.goto_first_child() {
+            continue;
+        }
+
+        let node = cursor.node();
+
+        // If we can't travel any further down, try going to next sibling and repeating
+        if cursor.goto_next_sibling() {
+            // If we succeed in going to the previous nodes sibling,
+            // we won't be encountering that node again, so we'll call if postorder
+            
+            print_node(node);
+            continue;
+        }
+
+        // Otherwise, we must travel back up; we'll loop until we reach the root or can
+        // go to the next sibling of a node again.
+        loop {
+            // Since we're retracing back up the tree, this is the last time we'll encounter
+            // this node, so we'll call if postorder
+            
+                print_node(node);
+            if !cursor.goto_parent() {
+                // We have arrived back at the root, so we are done.
+                return;
+            }
+
+            let node = cursor.node();
+
+            if cursor.goto_next_sibling() {
+                // If we succeed in going to the previous node's sibling,
+                // we will go back to travelling down that sibling's tree, and we also
+                // won't be encountering the previous node again, so we'll call if postorder
+                
+                print_node(node);
+                break;
+            }
+        }
+    }
     let query = Query::new(language, &"(
   (function_item (visibility_modifier) (identifier) @blo)
   (#eq? @blo foo)
 )").unwrap();
-    let mut cursor = QueryCursor::new();
-    let results = cursor
-        .matches(&query, tree.root_node(), lines)
-        .map(|m| {
-            println!("{:?}", m.captures[0].node);
-            (m.pattern_index, m.captures.to_owned())
-        })
-        .collect::<Vec<(usize, Vec<QueryCapture>)>>();
+    // let mut cursor = QueryCursor::new();
+    // let results = cursor
+    //     .matches(&query, tree.root_node(), lines)
+    //     .map(|m| {
+    //         println!("{:?}", m.captures[0].node);
+    //         (m.pattern_index, m.captures.to_owned())
+    //     })
+    //     .collect::<Vec<(usize, Vec<QueryCapture>)>>();
 
-    print!("{:?}", gen(tree.root_node()));
+    // print!("{:?}", gen(tree.root_node()));
 }
 
 fn get_grammar() -> GrammarJSON {
